cd21fe0d3b1e10cad711489a0c02a42c6525bae5
rayaneanjos <raydosanjos98@gmail.com>
2023-03-20 21:15:45 -0300

    Usando mais de um Reducer

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const redux = require('redux')
 const createStore = redux.createStore;
-
+const combineReducer = redux.combineReducers
 
 const incrementAction = (value)=>{return {type:'INCREMENT',payload:value || 1}}
 const decrementAction = (value)=>{return {type:'DECREMENT',payload:value || 1}}
@@ -16,9 +16,28 @@ function counterReducer(state = 5, action){
     }
 }
 
-const store = createStore(counterReducer)
+//-----------------------------------------------
+const addItemAction = (item)=>{return{type:'ADD_ITEM', payload: item}} 
+
+const listReducer = (state = ['Um item padrão'], action)=>{
+    switch (action.type) {
+        case 'ADD_ITEM':
+            return [...state, action.payload]
+    
+        default:
+            return state
+    }
+}
+
+const allReducers = combineReducer({
+    counter: counterReducer,
+    list: listReducer
+})
+
+const store = createStore(allReducers)
 console.log(store.getState())
 store.subscribe(()=>{console.log(store.getState())})
+store.dispatch(addItemAction('Um novo item'))
 store.dispatch(incrementAction())
 store.dispatch(incrementAction(1))
 store.dispatch(incrementAction(2))
